96691e00be81a94b6d75b67b72043bcf903f0147
wwbbx <wwbbx@hotmail.com>
2013-11-20 09:22:49 +0800

    Revert "adjust folder structure."
    This reverts commit 2421818a8a5d1f08da49c672fd9bccefbc51dafc.

--- UseRoseRegistrySetting.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-hubBroker = 'lhdevhubbroker.cos.agilent.com'
-office = '24br'
-
-command = 'ipy .\ChangeCrsRegistry.py -host {0} -office {1}'.format(hubBroker, office)
-subprocess.call(command, shell=True)
